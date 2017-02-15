3c52f8a2a59dc2591d889a0cf136defce527c5e1
mippet <pimluitjes@gmail.com>
2017-02-15 17:30:42 +0100

    took out recipes from reactApp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,10 @@
 // src/reducers/index.js
-import recipes from './recipes'
+
 import loadError from './load-error'
 import loading from './loading'
 import currentUser from './current-user'
 
 module.exports = {
-  recipes,
   loadError,
   loading,
   currentUser,
