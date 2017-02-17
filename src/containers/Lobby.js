f466afc204a83f554148bf7a66eaa90d43826e8d
mippet <pimluitjes@gmail.com>
2017-02-17 15:37:31 +0100

    added Join Button with NO actions

--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 import Title from '../components/Title'
 import { connect } from 'react-redux'
 import RaisedButton from 'material-ui/RaisedButton'
+import {List} from 'material-ui/List';
 
 import setupGames from '../actions/game/setupGames'
 import createGame from '../actions/game/create-game'
@@ -9,15 +10,18 @@ import createGame from '../actions/game/create-game'
 export class Lobby extends PureComponent {
   //after Lobby component is renderd, we add
   // the props that we got from SetUpgames to THIS component.
-  componentDidMount(){
-    this.prop.setupGames()
-  }
+  // componentDidMount(){
+  //   this.prop.setupGames()
+  // }
 
   render() {
     return(
       <div>
         <Title content="Game Lobby" />
         <RaisedButton label="Create Game" primary={true} onClick={ createGame} />
+        <List>
+          <RaisedButton label="Join Game" />
+        </List>
       </div>
     )
   }
@@ -26,4 +30,4 @@ export class Lobby extends PureComponent {
 //create NewGameButton
 //import JoinButton
 //import list of games
-export default (Lobby)
+export default connect(null,{setupGames, createGame})(Lobby)
