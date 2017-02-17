d289423eccf8c63703a634251d3b7788d736d6cd
mippet <pimluitjes@gmail.com>
2017-02-17 14:32:18 +0100

    added DidMount to component

--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -3,6 +3,11 @@ import Title from '../components/Title'
 import { connect } from 'react-redux'
 
 export class Lobby extends PureComponent {
+  //after Lobby component is renderd, we add
+  // the props that we got from SetUpgames to THIS component.
+  componentDidMount(){
+    this.prop.SetUpgames()
+  }
   render() {
     return(
       <div>
@@ -13,5 +18,6 @@ export class Lobby extends PureComponent {
 }
 
 //import NewGameButton
+//import JoinButton
 //import list of games
 export default (Lobby)
