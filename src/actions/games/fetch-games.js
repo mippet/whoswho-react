ad5a6ce5e50e01162e46399b1ed898d2cc35bb66
maja55 <majapodrug2@gmail.com>
2017-02-21 11:38:22 +0100

    updated Lobby state and fetch-games action

--- src/actions/games/fetch-games.js
+++ src/actions/games/fetch-games.js
@@ -8,30 +8,29 @@ export const FETCHED_GAMES = 'FETCHED_GAMES'
 const api = new API()
 const games = api.service('games')
 
-export default () => {
+export const fetchGames = () => {
   return (dispatch) => {
     dispatch(loading(true))
 console.log("fetching games... :)")
-
-    games.find()
     api.app.authenticate()
-    .then((result) => {
-      console.log('Results are in!', result)
-      dispatch(loadSuccess())
-      dispatch(fetchedGames(result))
+    .then(() => {
+        games.find({
+          query: {
+            $limit: 25
+          }
+        })
+      .then((response) => {
+        dispatch({
+          type: FETCHED_GAMES,
+          payload: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
     })
     .catch((error) => {
-      dispatch(loadError(error))
+      console.error(error)
     })
-    .then(() => {
-      dispatch(loading(false))
-    })
-  }
-}
-const fetchedGames = (result) => {
-  console.log('dispatching fetchedGames')
-  return {
-    type: FETCHED_GAMES,
-    payload: [].concat(result.data)
   }
 }
